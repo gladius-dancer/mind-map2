39974436443b219243ea63969f050975e082b315
Rustem <gladius.dancer@gmail.com>
2023-09-29 10:35:39 +0500

    Connection lines

--- src/components/Block/Block.tsx
+++ src/components/Block/Block.tsx
@@ -1,13 +1,12 @@
-import React, {MutableRefObject, RefObject, useEffect, useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import "./Block.css";
 import {ReactComponent as Plus} from "../../assets/icons/plus-solid.svg"
-import {ConnectionType} from "../ConnectionsContainer/types/ConnectionType";
 import {TreeType} from "../BlocksContainer/hooks/useAddChild";
-import {ConnectionsContainer} from "../ConnectionsContainer";
 
 type Content = {
     treeItem: TreeType;
     addChild: (a: string) => void;
+    addCords: (val: CordinateType[]) => void
 }
 
 type LocationType = {
@@ -15,61 +14,54 @@ type LocationType = {
     y: number;
 }
 
-type CordinateType = {
+export type CordinateType = {
     x: number;
     y: number;
 }
 
-
-function Block({treeItem, addChild}: Content) {
+function Block({treeItem, addChild, addCords}: Content) {
     const {version, label, childs} = treeItem;
+    const childRef = useRef<HTMLDivElement>(null);
+    const [parentCord, setParentCord] = useState<CordinateType>({x: 0, y: 0});
+    const [childCord, setChildCord] = useState([]);
 
-    const parentRef = useRef<HTMLDivElement>(null);
-    const [parentCor, setParentCor] = useState<CordinateType>({x: 0, y: 0});
-    const [cordinates, setCordinates] = useState<CordinateType[]>([]);
+    console.log(parentCord);
+
+    const getParentCordinate = (event: MouseEvent)=>{
+        setParentCord({x: event.clientX, y: event.clientY});
+    }
 
     const getCordinates = (element: HTMLDivElement): CordinateType => {
-        const parent = parentRef?.current?.getBoundingClientRect() as any;
-        return {x: parent.x, y: parent.y}
+        const parent = element.getBoundingClientRect();
+        return { x: parent.x, y: parent.y }
     };
 
     useEffect(() => {
-        const elCor: CordinateType[] = [];
-        const elements = document.querySelectorAll(`[data-id="0"]`)
-        elements.forEach((child, index) => {
-            // elCor.push(getCordinates(child as HTMLDivElement))
-            setCordinates(prev =>[...prev, getCordinates(child as HTMLDivElement)]) ;
+        const elements = document.querySelectorAll(`[data-id="0"]`);
+
+        elements.forEach((child) => {
+            const childCords = getCordinates(child as HTMLDivElement)
+            addCords([{x: parentCord.x, y: parentCord.y + 30}, {x: childCords.x + 180, y: childCords.y + 50}])
         })
-
-        setParentCor(getCordinates(parentRef.current as HTMLDivElement));
-
-
     }, []);
 
     return (
         <div className="block-layer">
-            {cordinates.map((cor, index) => (
-                <ConnectionsContainer
-                    key={index}
-                    start={parentCor}
-                    end={cor}
-                />
-            ))}
-
             <div className="block-container-column">
-                <div className="block" ref={parentRef}>
+                <div className="block" ref={childRef} onClick={(event:any)=>getParentCordinate(event)}>
                     <p>{label} {version}</p>
                     <span className="add" onClick={() => addChild(version)}>
                         <Plus className="plus"/>
                     </span>
                 </div>
-
                 <div className='.block-container-row'>
                     {childs?.map((item: TreeType) => (
                             <div key={item.version} data-id={treeItem.version}>
                                 <Block
+                                    key={item.version}
                                     treeItem={item}
                                     addChild={addChild}
+                                    addCords={addCords}
                                 />
                             </div>
                         )
--- src/components/BlocksContainer/BlocksContainer.tsx
+++ src/components/BlocksContainer/BlocksContainer.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./BlocksContainer.css";
 import {Block} from "../Block";
-import useAddChild, {TreeType} from "./hooks/useAddChild";
+import useAddChild from "./hooks/useAddChild";
+import {CordinateType} from "../Block/Block";
 
 function BlocksContainer() {
 
@@ -10,11 +11,53 @@ function BlocksContainer() {
         addChild
     } = useAddChild();
 
+    const [cords, setCords] = useState<CordinateType[][]>([]);
+
+    console.log(cords);
+
+    const distance = (start: CordinateType, end: CordinateType) => {
+        const dx = start.x - end.x;
+        const dy = start.y - end.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    const calculatePath = (start: CordinateType, end: CordinateType) => {
+        const center = {
+            x: (start.x + end.x) / 2,
+            y: (start.y + end.y) / 2,
+        };
+
+        const controlPoint = {
+            x: start.x + Math.min(distance(start, end), Math.abs(end.y - start.y) / 2, 150),
+            y: start.y,
+        };
+
+        return `
+          M ${start.x},${start.y} 
+          Q ${controlPoint.x}, ${controlPoint.y} ${center.x},${center.y} 
+          T ${end.x},${end.y}
+        `;
+    }
+
     return (
         <div className="block-container">
+            <svg className='block-image'>
+                <path d="M20,4L30,15z" className="path-arrow-top"/>
+                <path d="M20,26L30,15z" className="path-arrow-bottom"/>
+                {cords.map(itemCords => (
+                    <path
+                        d={calculatePath(itemCords[0], itemCords[1])}
+                        fill="transparent"
+                        stroke="red"
+                        strokeWidth="2"
+                    ></path>
+                ))}
+            </svg>
+
             <Block
                 treeItem={tree[0]}
                 addChild={addChild}
+                addCords={(val: CordinateType[]) => setCords([...cords, val])}
             />
         </div>
     );
